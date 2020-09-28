5eb0717961cddf585f48df35cc0bf75325264521
carsonchapman5 <70231784+carsonchapman5@users.noreply.github.com>
2020-09-28 13:09:10 -0700

    Add files via upload

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -15,7 +15,55 @@
 // =========================================================================== // 
 
 
+ namespace C137 {
+    void Morty(int start, int stop, int step) {
+        int i;
+        for (i = start; i <= stop; i += step) {
+             std::cout << i << ": ";
+           if (i % 15 == 0) {
+             std::cout << "Aww Geez Man" << std::endl;
+           }
+           else if (i % 5 == 0) {
+             std::cout << "Geez" << std::endl;
+           }
+           else if (i % 3 == 0) {
+             std::cout << "Aww" << std::endl;
+           }
+           std::cout << "\n";
+        }
+      }
+    void Morty(int start, int stop) {
+     int step = 1;
+     C137::Morty(start, stop, step);
+    }
+}
+  
+
+
+
 
 // =========================================================================== // 
 //          Define functions enclosed in Namespace Z286 here 
 // =========================================================================== // 
+namespace Z286 {
+  void Morty (int start, int stop, int step) {
+    int i;
+        for (i = start; i <= stop; i += step) {
+             std::cout << i << ": ";
+           if (i % 15 == 0) {
+             std::cout << "naM zeeG wwA" << std::endl;
+           }
+           else if (i % 5 == 0) {
+             std::cout << "zeeG" << std::endl;
+           }
+           else if (i % 3 == 0) {
+             std::cout << "wwA" << std::endl;
+           }
+           std::cout << "\n";
+        }
+    }
+  void Morty(int start, int stop) {
+     int step = 1;
+     Z286::Morty(start, stop, step);
+   }
+}
